c07b64e2882ad4123aca3023a3365c56df9a9813
Avraamiyy <a.parfenov@blissfinance.ru>
2021-06-07 21:22:51 +0300

    finish base framework functional

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ function jsLoaders() {
         {
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env']
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-proposal-class-properties']
             }
         }
     ]
